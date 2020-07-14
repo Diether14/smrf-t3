680337ffe12cf783ce51bef592f363145b88ad28
pogi <llenaresdiether@gmail.com>
2020-07-14 16:08:17 +0800

    ADD: VALIDATIONS FOR NEW ACTIVITY ROW

--- t3_appui/src/app/modals/add-row/add-row/add-row.component.ts
+++ t3_appui/src/app/modals/add-row/add-row/add-row.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActivityFactory } from 'src/app/classes/activity-factory';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-row',
@@ -36,7 +37,7 @@ export class AddRowComponent implements OnInit {
     }
   }
 
-  updateEndTime() {
+  updateStartTime() {
     if (this.startTime.length === 4 ) {
       const time = this.startTime.slice(0, 2);
       const min = this.startTime.slice(2, 4);
@@ -50,20 +51,66 @@ export class AddRowComponent implements OnInit {
     }
   }
 
+  updateEndTime() {
+    if (this.startTime.length === 4 ) {
+      const time = this.endTime.slice(0, 2);
+      const min = this.endTime.slice(2, 4);
+      const timeStr = moment(this.dateSelected).format('MM/DD/YYYY') + ' ' + time + ':' + min;
+      this.fullEndTime = moment(timeStr);
+    }
+  }
+
   addRow() {
     const act = this.activityFactory.createActivity({
       HEADER_ID       : this.activityService.headerObj.ID,
       START_TIME      : this.fullStartTime,
       END_TIME        : this.fullEndTime,
-      LAST_UPDATED_BY : null,
-      DATE_ENTERED    : this.activityService.servertime.format('DD/MMM/YY'),
-      DATE_UPDATED    : this.activityService.servertime.format('DD/MMM/YY'),
       IS_NEW          : 1,
-      IS_CHANGED      : 0
     });
-    if (this.activityService.isActivityAllowed(act.START_TIME)) {
+    console.log(act);
+    const validate = this.validate(act.START_TIME, act.END_TIME);
+    if (validate.isValid) {
       this.activityService.addCustomActivity(act);
+      this.activeModal.dismiss('success');
+    } else {
+      Swal.fire({
+        title: 'Warning',
+        text: validate.message,
+        icon: 'warning',
+        confirmButtonText: 'OK',
+      });
     }
+  }
+
+  validate(startTime: string, endTime: string) {
+    const mntStart = moment(startTime);
+    const mntEnd = moment(endTime);
+    let res = {
+      isValid: true,
+      message: 'Allowed'
+    };
+    if (!mntStart.isValid() || !mntEnd.isValid()) {
+      res = {
+        isValid: false,
+        message: 'Invalid Value'
+      };
+      return res;
+    }
+    if (!this.activityService.isActivityAllowed(startTime)) {
+      res = {
+        isValid: false,
+        message: 'An activity with given start time already exists'
+      };
+      return res;
+    }
+    if ((mntEnd.diff(mntStart.startOf('hour'), 'minutes')) > 60) {
+      res = {
+        isValid: false,
+        message: 'Invalid end time'
+      };
+      return res;
+    }
+    return res;
   }
 
 }
