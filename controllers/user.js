37a7b5d16272a8be259b21356561d6c33ac9f6e0
pogi <llenaresdiether@gmail.com>
2020-07-14 14:09:45 +0800

    ADD: FUNCTION TO TRANSFORM NAME TO UPPERCASE

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,9 @@ const user = require('../db_apis/user.js');
 const post = async (req, res, next) => {
     try {
         const data = {
-            last_name: req.body.last_name,
-            first_name: req.body.first_name,
-            middle_name: req.body.middle_name,
+            last_name: transform(req.body.last_name),
+            first_name: transform(req.body.first_name),
+            middle_name: transform(req.body.middle_name),
             username: req.body.username,
             user_level: req.body.user_level,
             department: req.body.department
@@ -20,3 +20,9 @@ const post = async (req, res, next) => {
 };
 
 module.exports.post = post;
+
+const transform = (string) => {
+    const arr = string.split('');
+    arr[0] = arr[0].toUpperCase();
+    return arr.join('');
+}
